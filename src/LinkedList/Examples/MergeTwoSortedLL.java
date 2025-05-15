b3709ab1380e675e38100da6647d449b8036163b
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-15 19:08:18 +0530

    Fixed bugs and updated code

--- src/LinkedList/Examples/MergeTwoSortedLL.java
+++ src/LinkedList/Examples/MergeTwoSortedLL.java
@@ -82,6 +82,7 @@ public class MergeTwoSortedLL {
     public void display() {
 
         Node temp = head;
+
         while (temp != null) {
             System.out.print(temp.value + " -> ");
             temp = temp.next;
